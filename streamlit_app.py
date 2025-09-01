c3c9f7121b2b4942b7a7af568db5987e8212e2d5
venkatlavu <venkatlavu1996@gmail.com>
2025-09-01 19:08:05 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.python.functions import col
+from snowflake.snowpark.session import Session
+from snowflake.snowpark.functions import col
+
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
