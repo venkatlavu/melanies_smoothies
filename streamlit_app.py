d247a494083fdf8bed9e3293fc11329f34d20433
venkatlavu <venkatlavu1996@gmail.com>
2025-09-01 19:47:59 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ cnx=st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
-
+st.write("Snowflake secrets:", st.secrets["snowflake"])
 ingredients_string = ''
 
 ingredients_list =st.multiselect('Choose upto 5 ingredients:'
@@ -41,5 +41,5 @@ if time_to_insert:
     session.sql(my_insert_stmt).collect()
     
     st.success('Your Smoothie is ordered!', icon="✅")
-  st.write("Snowflake secrets:", st.secrets["snowflake"])
+  
 
