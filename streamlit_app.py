32c62e06d1947318d1e50497a24b9b1c57696638
venkatlavu <venkatlavu1996@gmail.com>
2025-09-01 21:41:14 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,11 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
+
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
+
 ingredients_string = ''
 
 ingredients_list =st.multiselect('Choose upto 5 ingredients:'
