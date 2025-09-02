8e7f5524ab53824e5c8aed68f3ab8d8fee53b327
venkatlavu <venkatlavu1996@gmail.com>
2025-09-01 21:02:58 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,5 +46,5 @@ if time_to_insert:
 #new section to display nutrition information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
 
