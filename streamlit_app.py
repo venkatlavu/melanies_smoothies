040dde140b41c1704b66bc4cb5e974d744d6ecb5
venkatlavu <venkatlavu1996@gmail.com>
2025-09-03 10:17:29 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,12 +22,12 @@ pd_df=my_dataframe.to_pandas()
 st.dataframe(pd_df)
 st.stop()
 
-
+ ingredients_string = ''
 
 ingredients_list =st.multiselect('Choose upto 5 ingredients:'
                                  ,my_dataframe,max_selections=5)
 if ingredients_list:
-    ingredients_string = ''
+   
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
